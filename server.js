2034e05f0400d2321758532c7e77c38d380bacad
Tushar Singh <99042369+CodeTusharSingh@users.noreply.github.com>
2023-12-18 23:10:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cors({
   origin: 'http://localhost:3000',
   methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
