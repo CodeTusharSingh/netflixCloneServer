51dacd42798059aa8f6e45ad212ca9e3cdba11f5
Tushar Singh <99042369+CodeTusharSingh@users.noreply.github.com>
2023-12-18 19:17:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 // we link the router file to make our router easy
 app.use(Mainrouter)
