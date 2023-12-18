6a5bed1cbedf516be2e069c46eead043e275a8fe
Tushar Singh <99042369+CodeTusharSingh@users.noreply.github.com>
2023-12-19 00:52:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -834,10 +834,11 @@ app.post('/content/search', (req, res) => {
 
 app.get('/removeToken', (req, res) => {
   // Clear the existing tokens by setting expired tokens in the cookies
-  res.clearCookie('access_token',{ sameSite: 'None', secure: false, expires: new Date(0) });
-  res.clearCookie('Finish_token',{ sameSite: 'None', secure: false, expires: new Date(0) });
-  res.clearCookie('plan_token',{ sameSite: 'None', secure: false, expires: new Date(0) });
-  res.clearCookie('signed_token',{ sameSite: 'None', secure: false, expires: new Date(0) });
+  res.clearCookie('access_token');
+  res.clearCookie('Finish_token');
+  res.clearCookie('plan_token');
+  // res.clearCookie('signed_token');
+  cookies.set('signed_token', {expires: Date.now()});
 
   // Send a response indicating the token removal
   res.status(200).json({ msg: 'Cookies removed' });
